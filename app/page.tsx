e5ad5df23bd1b5055f24250da977957f099b68dd
estebanmpc <estebanmpc@icloud.com>
2024-08-05 00:02:17 -0400

    created outline, building navbar for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -2,112 +2,107 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-between px-6">
+      <video src="https://www.anduril.com/Home%20Masthead%20720.mp4" autoPlay loop playsInline id="headerVideo" className="min-h-screen min-w-max"></video>  
+      <div id="hero" className="mx-0 px-0 min-h-screen">
+        <div className="min-h-screen min-h-20">
+          <div className="flex items-start">
+            <div className>ANDRUIN</div>
+            <div className="bg-black">
+              <button  className="p-5">
+                <svg data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" className="bg-white">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path>
+                </svg>
+              </button>
+            </div>
+            {/* <nav>
+              <div>
+                <div>Lettuce Solutions</div>
+                <div>+</div>
+                <div>
+                  <ul>
+                    <li>Command & Control</li>
+                    <li>Mission Autonomy</li>
+                  </ul>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus voluptas optio dolor similique atque. Quaerat illo, consectetur ipsum harum maiores cum enim alias? Iure a veritatis doloribus provident nihil. Fugiat?</p>
+              <div>
+                <div>Farce Protection</div>
+                <div>+</div>
+                <div>
+                  <ul>
+                    <li>Counter UAS</li>
+                    <li>Counter Infusion</li>
+                    <li>Tea Counter Infusion</li>
+                  </ul>
+                </div>
+              </div>
+              <div>
+                <div>Air Systems</div>
+                <div>+</div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus voluptas optio dolor similique atque. Quaerat illo, consectetur ipsum harum maiores cum enim alias? Iure a veritatis doloribus provident nihil. Fugiat?</p>
+                <div>
+                  <ul>
+                    <li>Roadrunner</li>
+                    <li>Ghost</li>
+                    <li>ADFSF</li>
+                  </ul>
+                </div>
+              </div>
+              <div>
+                <div>Dive Vehicles</div>
+                <div>+</div>
+                <div>
+                  <ul>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus voluptas optio dolor similique atque. Quaerat illo, consectetur ipsum harum maiores cum enim alias? Iure a veritatis doloribus provident nihil. Fugiat?</p>
+                    <li>Dive</li>
+                  </ul>
+                </div>
+              </div>
+              <div>
+                <div>Racket Motor</div>
+                <div>+</div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus voluptas optio dolor similique atque. Quaerat illo, consectetur ipsum harum maiores cum enim alias? Iure a veritatis doloribus provident nihil. Fugiat?</p>
+              </div>
+              <div>Newsroom</div>
+              <div>Mission</div>
+            </nav> */}
+          </div>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        <h1>Our Mission</h1>
+        <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam aut laborum cumque maiores ipsa necessitatibus quisquam nemo consequuntur repellat. Dicta molestias cupiditate voluptatibus velit sequi officiis, voluptates laboriosam quibusdam ratione!
+        </p>
+        <div>
+          <a href="">Learn more about our mission</a>
+          {/* icon here */}
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      
+      <div>
+        <h1>Lettuce OS</h1>
+        <h2>Lorem ipsum dolor sit amet.</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam provident minima odio quae quisquam hic vel alias laborum voluptate tenetur!
+        </p>
+        <div>
+          <a href="">Learn more about command and control</a>
+          {/* icon here */}
+        </div>
+        <div>
+          <a href="">Learn more about mission autonomy</a>
+          {/* icon here */}
+        </div>
       </div>
+      
+
+
+
     </main>
   );
 }
